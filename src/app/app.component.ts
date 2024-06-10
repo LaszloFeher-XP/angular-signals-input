6ed1f1fe3297d5d0363f9d36d9d5813b11950498
Laszlo Feher <feher.laszlo@outlook.com>
2024-06-10 20:10:14 +0200

    Refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
 import {  Component, computed, signal } from '@angular/core';
-import { TodoItemComponent } from '../todo-item/todo-item.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TodoItemComponent, MatButtonModule ],
+  imports: [MatButtonModule, TodoItemComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   todoOneComplete = signal(false);
   todoTwoComplete = signal(false);
-  todoThreeComplete = signal(false);
+  todoThreeComplete = signal(false); 
 
   everyTodoIscompleted = computed(() => {
     return this.todoOneComplete() && this.todoTwoComplete() && this.todoThreeComplete()
